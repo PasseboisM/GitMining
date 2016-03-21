7185fa14694c5b96da5e3ef20b0511b752da3dee
wza14 <wza14@software.nju.edu.cn>
2016-03-21 22:02:52 +0800

    Comment：添加一波注释

--- project/gitmining/src/main/java/network/data/MassiveDataSourceDefault.java
+++ project/gitmining/src/main/java/network/data/MassiveDataSourceDefault.java
@@ -56,37 +56,19 @@ public class MassiveDataSourceDefault implements MassiveDataSource {
 		ObjChannel<String> channel = new ObjChannelWithBlockingQueue<String>();
 		MultiSourceSwitch<String> sourceSwitch = new BasicSourceSwitch<String>(channel);
 		String url = repoApi.makeRepoNamesApi();
-//		
-//		File f = new File("names.json");
-//		FileReader fr;
-//		String json = null;
-//		try {
-//			fr = new FileReader(f);
-//			BufferedReader br = new BufferedReader(fr);
-//			json = br.readLine();
-//			br.close();
-//			fr.close();
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 		
-		
-		
-		//获取并处理JSON
+		//获取并处理JSON，转化为Java的List对象
 		String json = conn.do_get(url);
 		Type listTypeType = new TypeToken<List<String>>(){}.getType();
 		List<String> repoLists = gson.fromJson(json, listTypeType);
-		
+		//分割列表为多个子列表，分别进行传输
 		List<String>[] listSplit = splitList(repoLists, SUGGESTED_THREAD_NUM);
-		
+		//创建过滤器，与用于传输子列表的管道一一对应，将所有传输子列表的管道连接到集流器中统一传输
 		PureDataTransFilter[] sources = new PureDataTransFilter[SUGGESTED_THREAD_NUM];
 		for(int i=0;i<SUGGESTED_THREAD_NUM;i++) {
 			sources[i] = new PureDataTransFilter<String>(listSplit[i], sourceSwitch);
 		}
+		//所有过滤器线程执行传输任务
 		execute(sources);
 		
 		return channel;
@@ -95,13 +77,10 @@ public class MassiveDataSourceDefault implements MassiveDataSource {
 
 	
 	public ObjChannel<RepositoryMin> getRepoMinInfo() throws NetworkException {
-		
 		return getRepo(RepositoryMin.class);
 	}
 	
-
 	public ObjChannel<Repository> getRepoInfo() throws NetworkException {
-				
 		return getRepo(Repository.class);
 	}
 
@@ -113,6 +92,12 @@ public class MassiveDataSourceDefault implements MassiveDataSource {
 		return getUser(GitUser.class);
 	}
 	
+	/**
+	 * 将大列表按照分裂个数分裂为多个子列表
+	 * @param origin 大列表
+	 * @param pieces 要求分裂个数
+	 * @return 子列表数组
+	 */
 	private <T> List[] splitList(List<T> origin, int pieces) {
 		assert pieces > 0;
 		
@@ -147,24 +132,31 @@ public class MassiveDataSourceDefault implements MassiveDataSource {
 	}
 	
 	private <T> ObjChannel<T> getRepo(Class<T> repoClass) throws NetworkException {
+		
+		//初始化传输Github项目名称的管道
 		ObjChannel<String> namesChannel = this.getRepoNames();
+		//初始化传输Github项目内容（Json格式）的管道
 		ObjChannel<String> JSONChannel = new ObjChannelWithBlockingQueue<String>();
 
 		
+		//初始化由项目名称向项目管道进行转化的过滤器、初始化集流器
 		GeneralProcessFilter[] repoJSONGetter = new GeneralProcessFilter[SUGGESTED_THREAD_NUM];
 		MultiSourceSwitch switchNameToJSON = new BasicSourceSwitch(JSONChannel);
 		for(int i=0;i<SUGGESTED_THREAD_NUM;i++) {
 			repoJSONGetter[i] = new RepoJSONGetter(namesChannel,switchNameToJSON,50);
 		}
+		//执行由项目名称向项目内容进行转化的线程
 		execute(repoJSONGetter);
 		
 		
+		//初始化项目内容对象（Java Object）的传输管道及由Json向JavaObject进行转化的过滤器数组
 		ObjChannel<T> objChannel = new ObjChannelWithBlockingQueue<T>();
 		JSONStringRPOFilter[] RPOSources = new JSONStringRPOFilter[SUGGESTED_THREAD_NUM];
 		MultiSourceSwitch switchJSONToObj = new BasicSourceSwitch(objChannel);
 		for(int i=0;i<SUGGESTED_THREAD_NUM;i++) {
 			RPOSources[i] = new JSONStringRPOFilter(JSONChannel, getBeans(repoClass), switchJSONToObj);
 		}
+		//执行由Json向JavaObject进行转化的过滤器线程
 		execute(RPOSources);
 		
 		return objChannel;
