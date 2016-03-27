ad71c264f103d77a7f2940474f3d778f0c5c6807
wza14 <wza14@software.nju.edu.cn>
2016-03-27 17:42:31 +0800

    Development：柱状图控件，Tooltip没加上，得找找资料

--- project/gitmining/src/main/java/presentation/Test.java
+++ project/gitmining/src/main/java/presentation/Test.java
@@ -6,28 +6,26 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import presentation.component.GitBarChart;
-import presentation.component.GitPieChart;
-import presentation.component.Line;
 
 public class Test extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		List<Double> datas = new ArrayList<>();
-		datas.add(1.0);
-		datas.add(3.0);
-		datas.add(5.0);
-		datas.add(7.0);
-		datas.add(9.0);
-		List<String> headers = new ArrayList<>(Arrays.asList("a","b","c","d","e"));
-		GitPieChart pieChart = new GitPieChart(headers, datas, "testPieChart");
+//		List<Double> datas = new ArrayList<>();
+//		datas.add(1.0);
+//		datas.add(3.0);
+//		datas.add(5.0);
+//		datas.add(7.0);
+//		datas.add(9.0);
+//		List<String> headers = new ArrayList<>(Arrays.asList("a","b","c","d","e"));
+//		GitPieChart pieChart = new GitPieChart(headers, datas, "testPieChart");
 		
-		
-//		GitBarChart barChart = new GitBarChart(null, null, "TestBarChart", "xLabel", "yLabel");
-		Scene scene = new Scene(pieChart, 800, 600);
+		List<String> headers = new ArrayList<>(Arrays.asList("a","b","c","d","e","f","g"));
+		List<Number> numbers = new ArrayList<>(Arrays.asList(58,69.3,100,321.2,26,233,21.5));
+		GitBarChart barChart = new GitBarChart(headers, numbers, "SName","TestBarChart", "xLabel", "yLabel");
+		Scene scene = new Scene(barChart, 800, 600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
