3ba44bccf81166f2348f758f9727275571c5d007
wza14 <wza14@software.nju.edu.cn>
2016-03-27 19:49:40 +0800

    Development：柱状图控件添加Tooltip

---
+++ project/gitmining/src/main/java/presentation/Test.java
@@ -0,0 +1,39 @@
+package presentation;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import presentation.component.GitBarChart;
+
+public class Test extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+//		List<Double> datas = new ArrayList<>();
+//		datas.add(1.0);
+//		datas.add(3.0);
+//		datas.add(5.0);
+//		datas.add(7.0);
+//		datas.add(9.0);
+//		List<String> headers = new ArrayList<>(Arrays.asList("a","b","c","d","e"));
+//		GitPieChart pieChart = new GitPieChart(headers, datas, "testPieChart");
+		
+		List<String> headers = new ArrayList<>(Arrays.asList("a","b","c","d","e","f","g"));
+		List<Number> numbers = new ArrayList<>(Arrays.asList(58,69.3,100,321.2,26,233,21.5));
+		GitBarChart barChart = new GitBarChart(headers, numbers, "SName","TestBarChart", "xLabel", "yLabel");
+		Scene scene = new Scene(barChart, 800, 600);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+}
--- project/gitmining/src/main/java/presentation/component/GitBarChart.java
+++ project/gitmining/src/main/java/presentation/component/GitBarChart.java
@@ -5,25 +5,28 @@ import java.util.List;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.AnchorPane;
 
 public class GitBarChart extends AnchorPane {
 	@FXML
-	private BarChart<String,Number> barChart;
+	private BarChart<String, Number> barChart;
 	@FXML
 	private CategoryAxis xAxis;
 	@FXML
 	private NumberAxis yAxis;
-	
 
 	/**
 	 * 柱状图构造函数
 	 */
-	public GitBarChart(List<String> columns, List<Number> values,String seriesName,String title,String xLabel,String yLabel) {
+	public GitBarChart(List<String> columns, List<Number> values, String seriesName, String title, String xLabel,
+			String yLabel) {
 		FXMLLoader fxmlLoader = new FXMLLoader(GitBarChart.class.getResource("barChart.fxml"));
 		fxmlLoader.setController(this);
 		fxmlLoader.setRoot(this);
@@ -35,9 +38,8 @@ public class GitBarChart extends AnchorPane {
 		barChart.setTitle(title);
 		xAxis.setLabel(xLabel);
 		yAxis.setLabel(yLabel);
-		this.initial(columns,values,seriesName);
+		this.initial(columns, values, seriesName);
 	}
-	
 
 	/**
 	 * 初始化柱状图
@@ -45,18 +47,21 @@ public class GitBarChart extends AnchorPane {
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private void initial(List<String> columns, List<Number> values, String seriesName) {
 		XYChart.Series series = new XYChart.Series();
-        series.setName(seriesName);
-        for (int i = 0; i < columns.size(); i++) {
-        	XYChart.Data data =new XYChart.Data(columns.get(i),values.get(i));
-            series.getData().add(data);
-        }
-//        Node node = data.getNode();
-//        Tooltip tooltip = new Tooltip(String.valueOf(999));
-//        yAxis.get
-//		Tooltip.install(xAxis, tooltip);
-        barChart.getData().add(series);
-        barChart.setCategoryGap(500/columns.size());
+		series.setName(seriesName);
+		for (int i = 0; i < columns.size(); i++) {
+			XYChart.Data data = new XYChart.Data(columns.get(i), values.get(i));
+			series.getData().add(data);
+		}
+		barChart.getData().add(series);
+		barChart.setCategoryGap(500 / columns.size());
 
-    }
+		for (int i = 0; i < columns.size(); i++) {
+			XYChart.Data data = (Data) series.getData().get(i);
+			Node node = data.getNode();
+			Tooltip tooltip = new Tooltip(String.valueOf(values.get(i)));
+			Tooltip.install(node, tooltip);
+		}
+
+	}
 
 }
