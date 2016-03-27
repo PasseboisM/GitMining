3dcbb13206d0f88c4aeb431c4354c2e1bcb0c69f
xjh14 <xjh14@software.nju.edu.cn>
2016-03-27 14:45:58 +0800

    Developement: 新建逻辑层统计模块接口组织。还未具体填充、实现。

---
+++ project/gitmining/src/main/java/logic/calc/service/GeneralStatisticsService.java
@@ -0,0 +1,15 @@
+package logic.calc.service;
+
+
+/**
+ * 提供对本系统内所有数据进行整体特征分析的服务接口。<br />
+ * 一般不涉及对某一具体的Repository，GitUser的分析。
+ * @author xjh14
+ * Ver: 0.1
+ * Created at: 2016年3月27日
+ */
+public interface GeneralStatisticsService {
+
+	
+	
+}
---
+++ project/gitmining/src/main/java/logic/calc/service/RepositoryStatisticsService.java
@@ -0,0 +1,14 @@
+package logic.calc.service;
+
+/**
+ * 用于为单个仓库进行数据分析、统计的服务接口。<br />
+ * 一般接受的参数为一个Repository。
+ * @author xjh14
+ * Ver: 0.1
+ * Created at: 2016年3月27日
+ */
+public interface RepositoryStatisticsService {
+
+	
+	
+}
