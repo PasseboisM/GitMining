f4f07ce9c25c62ff1c7512a11df0d68df39f426d
wza14 <wza14@software.nju.edu.cn>
2016-03-27 14:17:12 +0800

    Other；去警告

--- project/gitmining/src/main/java/presentation/component/Bar.java
+++ project/gitmining/src/main/java/presentation/component/Bar.java
@@ -1,22 +1,15 @@
 package presentation.component;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
 public class Bar extends AnchorPane {
 	@FXML
