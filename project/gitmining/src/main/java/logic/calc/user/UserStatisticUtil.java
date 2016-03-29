c0e7683809ea83b8dd90b3c3100765cd0d602977
xjh14 <xjh14@software.nju.edu.cn>
2016-03-29 11:53:41 +0800

    Development: 进行Python模块修改。

--- project/gitmining/src/main/java/logic/calc/user/UserStatisticUtil.java
+++ project/gitmining/src/main/java/logic/calc/user/UserStatisticUtil.java
@@ -12,7 +12,7 @@ import logic.calc.python.PythonRunner;
 public class UserStatisticUtil {
 	private static final String SINGLE_USER_FILE =  "statistic_single_user_rank.py";
 	public static RadarDatas getSingleUserPoints(String name) throws IOException, InterruptedException{
-		List<String> strResult = PythonRunner.runpython(SINGLE_USER_FILE, name);
+		List<String> strResult = PythonRunner.runPython(SINGLE_USER_FILE, name);
 		RadarDatas radarDatas = new UserRanks();
 		List<String> headers = new ArrayList<>(Arrays.asList("followers", "following", "public_gists", "public_repos"));
 		for (int i = 0; i < headers.size(); i++) {
