31028d815877ae6ad001d2763e23ed94495a82b2
wza14 <wza14@software.nju.edu.cn>
2016-04-10 21:36:15 +0800

    Development：初始化文字

--- project/gitmining/src/main/java/presentation/component/WaitLoader.java
+++ project/gitmining/src/main/java/presentation/component/WaitLoader.java
@@ -1,7 +1,9 @@
 package presentation.component;
 
+import java.awt.Label;
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 
@@ -15,8 +17,10 @@ public class WaitLoader extends AnchorPane{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		this.text.setText(text);
 	}
 	public WaitLoader(){
 		this("  数据加载中...");
 	}
+	@FXML private Label text;
 }
--- project/gitmining/src/main/java/presentation/ui/search/DetailPane.java
+++
@@ -1,8 +0,0 @@
-package presentation.ui.search;
-
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-
-public interface DetailPane {
-	public Pane getInstance(AnchorPane rightComponentParent);
-}
