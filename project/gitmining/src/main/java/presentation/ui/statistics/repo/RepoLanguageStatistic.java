e2927d0c8e92055572481e9717961a89c115b0ea
sj14 <sj14@software.nju.edu.cn>
2016-04-11 22:03:19 +0800

    Development：图表界面添加背景图

--- project/gitmining/src/main/java/presentation/ui/statistics/repo/RepoLanguageStatistic.java
+++ project/gitmining/src/main/java/presentation/ui/statistics/repo/RepoLanguageStatistic.java
@@ -1,14 +1,18 @@
 package presentation.ui.statistics.repo;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 import chart_data.RepoDistOverLanguage;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import logic.calc.general.GeneralStatisticsUtil;
 import logic.calc.service.GeneralStatisticsService;
 import presentation.component.GitPieChart;
+import presentation.image.ImageFactory;
 import presentation.ui.statistics.StatisticsPane;
 
 /**
@@ -31,10 +35,32 @@ public class RepoLanguageStatistic implements StatisticsPane {
 
 	private void initial(AnchorPane rightComponentParent) {
 		this.initialChart();
+		bgImage=loadImgFile();
+		initialImage();
 	}
 
 	@FXML
 	private AnchorPane anchorPane;
+	private ImageView image;
+	private static Image bgImage = null;
+	
+	private Image loadImgFile() {
+		Image tempImage = null;
+		String imageFilename ="Background.jpg";
+		try {
+			tempImage = ImageFactory.getImageByFileName(imageFilename);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+	private void initialImage() {
+		image = new ImageView();
+		image.setImage(bgImage);
+		image.setFitWidth(1050);
+		image.setFitHeight(675);
+		anchorPane.getChildren().add(0,image);
+	}
 
 	public void initialChart() {
 		GeneralStatisticsService statisticsService = new GeneralStatisticsUtil();
