f34120262ae72f5c159f7edfc503067f77ba988e
sj14 <sj14@software.nju.edu.cn>
2016-04-11 22:36:47 +0800

    Development：图表背景图添加完了

--- project/gitmining/src/main/java/presentation/ui/statistics/user/UserCreateTimeStatisticsPane.java
+++ project/gitmining/src/main/java/presentation/ui/statistics/user/UserCreateTimeStatisticsPane.java
@@ -1,14 +1,18 @@
 package presentation.ui.statistics.user;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 import chart_data.UserDistOverCreateTime;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import logic.calc.general.GeneralStatisticsUtil;
 import logic.calc.service.GeneralStatisticsService;
 import presentation.component.GitLineChart;
+import presentation.image.ImageFactory;
 import presentation.ui.statistics.StatisticsPane;
 
 public class UserCreateTimeStatisticsPane implements StatisticsPane {
@@ -27,12 +31,34 @@ public class UserCreateTimeStatisticsPane implements StatisticsPane {
 	}
 	private void initial(AnchorPane rightComponentParent) {
 		this.initialChart();
+		bgImage=loadImgFile();
+		initialImage();
 	}
 	
 	@FXML
 	private AnchorPane anchorPane;
+	private ImageView image;
+	private static Image bgImage = null;
 	
 //	private AnchorPane rightComponentParent;
+	
+	private Image loadImgFile() {
+		Image tempImage = null;
+		String imageFilename ="Background.jpg";
+		try {
+			tempImage = ImageFactory.getImageByFileName(imageFilename);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+	private void initialImage() {
+		image = new ImageView();
+		image.setImage(bgImage);
+		image.setFitWidth(1050);
+		image.setFitHeight(675);
+		anchorPane.getChildren().add(0,image);
+	}
 	
 	public void initialChart(){
 		GeneralStatisticsService statisticsService = new GeneralStatisticsUtil();
--- project/gitmining/src/main/java/presentation/ui/statistics/user/UserFollowerStatisticsPane.java
+++ project/gitmining/src/main/java/presentation/ui/statistics/user/UserFollowerStatisticsPane.java
@@ -1,14 +1,18 @@
 package presentation.ui.statistics.user;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 import chart_data.UserDistOverFollower;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import logic.calc.general.GeneralStatisticsUtil;
 import logic.calc.service.GeneralStatisticsService;
 import presentation.component.GitLineChart;
+import presentation.image.ImageFactory;
 import presentation.ui.statistics.StatisticsPane;
 
 public class UserFollowerStatisticsPane implements StatisticsPane {
@@ -28,10 +32,32 @@ public class UserFollowerStatisticsPane implements StatisticsPane {
 
 	private void initial(AnchorPane rightComponentParent) {
 		this.initialChart();
+		bgImage=loadImgFile();
+		initialImage();
 	}
 
 	@FXML
 	private AnchorPane anchorPane;
+	private ImageView image;
+	private static Image bgImage = null;
+	
+	private Image loadImgFile() {
+		Image tempImage = null;
+		String imageFilename ="Background.jpg";
+		try {
+			tempImage = ImageFactory.getImageByFileName(imageFilename);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+	private void initialImage() {
+		image = new ImageView();
+		image.setImage(bgImage);
+		image.setFitWidth(1050);
+		image.setFitHeight(675);
+		anchorPane.getChildren().add(0,image);
+	}
 
 	public void initialChart() {
 		GeneralStatisticsService generalStatisticsService = new GeneralStatisticsUtil();
