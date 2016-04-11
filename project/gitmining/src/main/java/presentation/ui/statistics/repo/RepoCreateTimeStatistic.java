3080b7973b5c9f3f77e2f353b54956ac2bca321e
sj14 <sj14@software.nju.edu.cn>
2016-04-11 16:51:48 +0800

    Development：图表界面添加图片

--- project/gitmining/src/main/java/presentation/ui/statistics/repo/RepoCreateTimeStatistic.java
+++ project/gitmining/src/main/java/presentation/ui/statistics/repo/RepoCreateTimeStatistic.java
@@ -1,14 +1,18 @@
 package presentation.ui.statistics.repo;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 import chart_data.RepoDistOverCreateTime;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import logic.calc.general.GeneralStatisticsUtil;
 import logic.calc.service.GeneralStatisticsService;
 import presentation.component.GitBarChart;
+import presentation.image.ImageFactory;
 import presentation.ui.statistics.StatisticsPane;
 
 public class RepoCreateTimeStatistic implements StatisticsPane {
@@ -29,11 +33,33 @@ public class RepoCreateTimeStatistic implements StatisticsPane {
 	private void initial(AnchorPane rightComponentParent) {
 		this.initialChart();
 		// this.rightComponentParent=rightComponentParent;
+		bgImage=loadImgFile();
+		initialImage();
 	}
 
 	@FXML
 	private AnchorPane anchorPane;
 	// private AnchorPane rightComponentParent;
+    private ImageView image;
+	private static Image bgImage = null;
+	
+	private Image loadImgFile() {
+		Image tempImage = null;
+		String imageFilename ="Background.jpg";
+		try {
+			tempImage = ImageFactory.getImageByFileName(imageFilename);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+	private void initialImage() {
+		image = new ImageView();
+		image.setImage(bgImage);
+		image.setFitWidth(1050);
+		image.setFitHeight(675);
+		anchorPane.getChildren().add(0,image);
+	}
 
 	public void initialChart() {
 		GeneralStatisticsService statisticsService = new GeneralStatisticsUtil();
