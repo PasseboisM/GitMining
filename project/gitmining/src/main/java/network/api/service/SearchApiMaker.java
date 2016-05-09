ef81cf59a705fe1b896f3138e7a4db9efe457518
wza14 <wza14@software.nju.edu.cn>
2016-05-09 20:19:07 +0800

    Other：添加依赖

---
+++ project/gitmining/src/main/java/network/api/service/SearchApiMaker.java
@@ -0,0 +1,10 @@
+package network.api.service;
+
+import common.param_obj.UserSearchParam;
+
+public interface SearchApiMaker {
+	static final String SEARCH_SITE = "https://api.github.com/search";
+	static final String USER = "/users";
+	static final String REPO = "/repositories";
+	public String makeSearchUserApi(UserSearchParam param);
+}
