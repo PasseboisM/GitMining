cd4f6823b5611c9f14c38a9a50be93d52dfac190
xjh14 <xjh14@software.nju.edu.cn>
2016-05-15 16:25:01 +0800

    Development: 借助原项目的代码，将本地数据加入到了数据库。

---
+++ project/gitminingserver/src/data/manage/DataServiceMakerDefault.java
@@ -0,0 +1,28 @@
+package data.manage;
+
+import data.service.DataServiceMaker;
+import data.service.RepositoryRequestHandler;
+import data.service.StatiticRequestHandler;
+import data.service.UserRequestHandler;
+
+public class DataServiceMakerDefault extends DataServiceMaker {
+
+	@Override
+	public RepositoryRequestHandler getRepositoryService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserRequestHandler getUserService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StatiticRequestHandler getStatisticHandler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ project/gitminingserver/src/data/service/StatiticRequestHandler.java
@@ -0,0 +1,5 @@
+package data.service;
+
+public abstract class StatiticRequestHandler {
+
+}
