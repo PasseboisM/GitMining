e40b7580314225a6161af72f9e721b6a01222444
wza14 <wza14@software.nju.edu.cn>
2016-05-27 11:51:29 +0800

    Development：添加所有图表，测试js

--- project/gitminingweb/WebContent/js/echart.js
+++ project/gitminingweb/WebContent/js/echart.js
@@ -73,6 +73,7 @@ $(function() {
                     yAxis : [
                     {
                         type : 'value'
+                        formatter: '{value} 人'
                     }
                     ],
                     series : [
@@ -168,7 +169,8 @@ $(function() {
                     ],
                     yAxis : [
                     {
-                        type : 'value'
+                        type : 'value'，
+                        formatter: '{value} 个'
                     }
                     ],
                     series : [
@@ -267,7 +269,8 @@ $(function() {
                     ],
                     yAxis : [
                     {
-                        type : 'value'
+                        type : 'value'，
+                        formatter: '{value} 个'
                     }
                     ],
                     series : [
@@ -305,6 +308,409 @@ $(function() {
             }
         });
 
+        $.ajax(url, {
+            type : 'GET',
+            // async : false,
+            // contentType : 'application/json',
+            // dataType : 'json',
+            data : {type:'general',param:'RepoDistOverCreateTime'},
+            success : function(data) {
+                // Set up the chart
+                var ranges = JSON.parse(data).counts;
+                var catagories = []
+                var showdatas = []
+                for (var i = 0; i < ranges.length; i++) {
+                    var range = ranges[i];
+                    catagories[i] = range.timeLo+"to"+range.timeHi;
+                    showdatas[i] = range.count;
+                }
+
+                // 使用
+                require(
+                    [
+                    'echarts',
+                    'echarts/chart/bar',
+                    'echarts/chart/line' 
+                    ],
+                function (ec) {
+                // 基于准备好的dom，初始化echarts图表
+                var myChart = ec.init(document.getElementById('repo_time')); 
+               
+                var option = {
+                    title : {
+                        text: '项目创建时间统计图表',
+                        // subtext: '纯属虚构'
+                    },
+                    tooltip : {
+                        trigger: 'axis'
+                    },
+                    //legend: {
+                        // data:['蒸发量']
+                    //},
+                    toolbox: {
+                        show : true,
+                        feature : {
+                            mark : {show: true},
+                            dataView : {show: true, readOnly: false},
+                            magicType : {show: true, type: ['line', 'bar']},
+                            restore : {show: true},
+                            saveAsImage : {show: true}
+                        }
+                    },
+                    calculable : true,
+                    xAxis : [
+                    {
+                        type : 'category',
+                        data : catagories,
+                        axisLabel :{  
+                            interval:0 ,
+                            rotate: -40
+                        }
+                    }
+                    ],
+                    yAxis : [
+                    {
+                        type : 'value'，
+                        formatter: '{value} 个'
+                    }
+                    ],
+                    series : [
+                    {
+                        name:'项目个数',
+                        type:'bar',
+                        data:showdatas,
+                        markPoint : {
+                            data : [
+                            {type : 'max', name: '最大值'},
+                            {type : 'min', name: '最小值'}
+                            ]
+                        },
+                        markLine : {
+                            data : [
+                            {type : 'average', name: '平均值'}
+                            ]
+                        }
+                    },
+
+                    ],
+                    color: [ 
+                            '#1e90ff', '#87cefa', '#da70d6', '#32cd32', '#6495ed', 
+                            '#ff69b4', '#ba55d3', '#cd5c5c', '#ffa500', '#40e0d0', 
+                            '#ff7f50', '#ff6347', '#7b68ee', '#00fa9a', '#ffd700', 
+                            '#6b8e23', '#ff00ff', '#3cb371', '#b8860b', '#30e0e0' 
+                        ]
+                };
+
+                // 为echarts对象加载数据 
+                myChart.setOption(option); 
+            }
+            );
+                
+            }
+        });
+
+        $.ajax(url, {
+            type : 'GET',
+            // async : false,
+            // contentType : 'application/json',
+            // dataType : 'json',
+            data : {type:'general',param:'UserDistOverCreateTime'},
+            success : function(data) {
+                // Set up the chart
+                var ranges = JSON.parse(data).counts;
+                var catagories = []
+                var showdatas = []
+                for (var i = 0; i < ranges.length; i++) {
+                    var range = ranges[i];
+                    catagories[i] = range.timeLo+"~"+range.timeHi;
+                    showdatas[i] = range.count;
+                }
+
+                // 使用
+                require(
+                    [
+                    'echarts',
+                    'echarts/chart/bar',
+                    'echarts/chart/line' 
+                    ],
+                function (ec) {
+                // 基于准备好的dom，初始化echarts图表
+                var myChart = ec.init(document.getElementById('user_time')); 
+               
+                var option = {
+                    title : {
+                        text: '用户创建时间统计图表',
+                        // subtext: '纯属虚构'
+                    },
+                    tooltip : {
+                        trigger: 'axis'
+                    },
+                    //legend: {
+                        // data:['蒸发量']
+                    //},
+                    toolbox: {
+                        show : true,
+                        feature : {
+                            mark : {show: true},
+                            dataView : {show: true, readOnly: false},
+                            magicType : {show: true, type: ['line', 'bar']},
+                            restore : {show: true},
+                            saveAsImage : {show: true}
+                        }
+                    },
+                    calculable : true,
+                    xAxis : [
+                    {
+                        type : 'category',
+                        data : catagories,
+                        axisLabel :{  
+                            interval:0 ,
+                            rotate: -40
+                        }
+                    }
+                    ],
+                    yAxis : [
+                    {
+                        type : 'value'，
+                        formatter: '{value} 人'
+                    }
+                    ],
+                    series : [
+                    {
+                        name:'用户数',
+                        type:'bar',
+                        data:showdatas,
+                        markPoint : {
+                            data : [
+                            {type : 'max', name: '最大值'},
+                            {type : 'min', name: '最小值'}
+                            ]
+                        },
+                        markLine : {
+                            data : [
+                            {type : 'average', name: '平均值'}
+                            ]
+                        }
+                    },
+
+                    ],
+                    color: [ 
+                            '#1e90ff', '#87cefa', '#da70d6', '#32cd32', '#6495ed', 
+                            '#ff69b4', '#ba55d3', '#cd5c5c', '#ffa500', '#40e0d0', 
+                            '#ff7f50', '#ff6347', '#7b68ee', '#00fa9a', '#ffd700', 
+                            '#6b8e23', '#ff00ff', '#3cb371', '#b8860b', '#30e0e0' 
+                        ]
+                };
+
+                // 为echarts对象加载数据 
+                myChart.setOption(option); 
+            }
+            );
+                
+            }
+        });
+
+        $.ajax(url, {
+            type : 'GET',
+            // async : false,
+            // contentType : 'application/json',
+            // dataType : 'json',
+            data : {type:'general',param:'RepoDistOverStar'},
+            success : function(data) {
+                // Set up the chart
+                var ranges = JSON.parse(data).counts;
+                var catagories = []
+                var showdatas = []
+                for (var i = 0; i < ranges.length; i++) {
+                    var range = ranges[i];
+                    catagories[i] = range.lowerStar+"~"+range.higherStar;
+                    showdatas[i] = range.numOfRepos;
+                }
+
+                // 使用
+                require(
+                    [
+                    'echarts',
+                    'echarts/chart/bar',
+                    'echarts/chart/line' 
+                    ],
+                function (ec) {
+                // 基于准备好的dom，初始化echarts图表
+                var myChart = ec.init(document.getElementById('star')); 
+               
+                var option = {
+                    title : {
+                        text: '项目关注统计图表',
+                        // subtext: '纯属虚构'
+                    },
+                    tooltip : {
+                        trigger: 'axis'
+                    },
+                    //legend: {
+                        // data:['蒸发量']
+                    //},
+                    toolbox: {
+                        show : true,
+                        feature : {
+                            mark : {show: true},
+                            dataView : {show: true, readOnly: false},
+                            magicType : {show: true, type: ['line', 'bar']},
+                            restore : {show: true},
+                            saveAsImage : {show: true}
+                        }
+                    },
+                    calculable : true,
+                    xAxis : [
+                    {
+                        type : 'category',
+                        data : catagories,
+                        axisLabel :{  
+                            interval:0 ,
+                            rotate: -40
+                        }
+                    }
+                    ],
+                    yAxis : [
+                    {
+                        type : 'value'，
+                        formatter: '{value} 个'
+                    }
+                    ],
+                    series : [
+                    {
+                        name:'项目数',
+                        type:'bar',
+                        data:showdatas,
+                        markPoint : {
+                            data : [
+                            {type : 'max', name: '最大值'},
+                            {type : 'min', name: '最小值'}
+                            ]
+                        },
+                        markLine : {
+                            data : [
+                            {type : 'average', name: '平均值'}
+                            ]
+                        }
+                    },
+
+                    ],
+                    color: [ 
+                            '#1e90ff', '#87cefa', '#da70d6', '#32cd32', '#6495ed', 
+                            '#ff69b4', '#ba55d3', '#cd5c5c', '#ffa500', '#40e0d0', 
+                            '#ff7f50', '#ff6347', '#7b68ee', '#00fa9a', '#ffd700', 
+                            '#6b8e23', '#ff00ff', '#3cb371', '#b8860b', '#30e0e0' 
+                        ]
+                };
+
+                // 为echarts对象加载数据 
+                myChart.setOption(option); 
+            }
+            );
+                
+            }
+        });
+
+        $.ajax(url, {
+            type : 'GET',
+            // async : false,
+            // contentType : 'application/json',
+            // dataType : 'json',
+            data : {type:'general',param:'UserDistOverType'},
+            success : function(data) {
+                // Set up the chart
+                var ranges = JSON.parse(data).counts;
+                var catagories = []
+                var showdatas = []
+                for (var i = 0; i < ranges.length; i++) {
+                    var range = ranges[i];
+                    catagories[i] = range.type;
+                    showdatas[i] = range.count;
+                }
+
+                // 使用
+                require(
+                    [
+                    'echarts',
+                    'echarts/chart/bar',
+                    'echarts/chart/line' 
+                    ],
+                function (ec) {
+                // 基于准备好的dom，初始化echarts图表
+                var myChart = ec.init(document.getElementById('type')); 
+               
+                var option = {
+                    title : {
+                        text: '项目语言统计图表',
+                        // subtext: '纯属虚构'
+                    },
+                    tooltip : {
+                        trigger: 'axis'
+                    },
+                    //legend: {
+                        // data:['蒸发量']
+                    //},
+                    toolbox: {
+                        show : true,
+                        feature : {
+                            mark : {show: true},
+                            dataView : {show: true, readOnly: false},
+                            magicType : {show: true, type: ['line', 'bar']},
+                            restore : {show: true},
+                            saveAsImage : {show: true}
+                        }
+                    },
+                    calculable : true,
+                    xAxis : [
+                    {
+                        type : 'category',
+                        data : catagories,
+                        axisLabel :{  
+                            interval:0 ,
+                            rotate: -40
+                        }
+                    }
+                    ],
+                    yAxis : [
+                    {
+                        type : 'value'，
+                        formatter: '{value} 个'
+                    }
+                    ],
+                    series : [
+                    {
+                        name:'用户数',
+                        type:'bar',
+                        data:showdatas,
+                        markPoint : {
+                            data : [
+                            {type : 'max', name: '最大值'},
+                            {type : 'min', name: '最小值'}
+                            ]
+                        },
+                        markLine : {
+                            data : [
+                            {type : 'average', name: '平均值'}
+                            ]
+                        }
+                    },
+
+                    ],
+                    color: [ 
+                            '#1e90ff', '#87cefa', '#da70d6', '#32cd32', '#6495ed', 
+                            '#ff69b4', '#ba55d3', '#cd5c5c', '#ffa500', '#40e0d0', 
+                            '#ff7f50', '#ff6347', '#7b68ee', '#00fa9a', '#ffd700', 
+                            '#6b8e23', '#ff00ff', '#3cb371', '#b8860b', '#30e0e0' 
+                        ]
+                };
+
+                // 为echarts对象加载数据 
+                myChart.setOption(option); 
+            }
+            );
+                
+            }
+        });
     });
 
 });
