e5f79b5e834ce1ddaaff9d515fccff7dce70b9b8
wza14 <wza14@software.nju.edu.cn>
2016-05-27 09:18:36 +0800

    Development：添加项目图表

--- project/gitminingweb/WebContent/js/chart.js
+++ project/gitminingweb/WebContent/js/chart.js
@@ -59,6 +59,62 @@ $(function() {
 				});
 			}
 		});
+
+		$.ajax(url, {
+			type : 'GET',
+			// async : false,
+			// contentType : 'application/json',
+			// dataType : 'json',
+			data : {type:'general',param:'RepoDistOverFork'},
+			success : function(data) {
+				// Set up the chart
+				var ranges = JSON.parse(data).ranges;
+				var catagories = []
+				var showdatas = []
+				for (var i = 0; i < ranges.length; i++) {
+					var range = ranges[i];
+					catagories[i] = range.lowerBound+"~"+range.higherBound;
+					showdatas[i] = range.numOfRepos;
+				}
+				var companychart = new Highcharts.Chart({
+					chart : {
+						renderTo : 'fork',
+						type : 'column',
+						margin : 100,
+						options3d : {
+							enabled : true,
+							alpha : 5,
+							beta : 15,
+							depth : 50,
+							viewDistance : 25
+						}
+					},
+					title : {
+						text : '项目复刻数'
+					},
+					plotOptions : {
+						column : {
+							depth : 25
+						}
+					},
+					xAxis : {
+						categories : catagories
+					},
+					yAxis : {
+						title : {
+							text : '项目个数'
+						},
+					},
+					tooltip : {
+						valueSuffix : '个'
+					},
+					series : [ {
+						name : '项目',
+						data : showdatas
+					} ]
+				});
+			}
+		});
 	});
 
 });
