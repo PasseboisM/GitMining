10730f9ec5dcd8ab80e9fb41799447588a7b4c83
wza14 <wza14@software.nju.edu.cn>
2016-05-31 10:47:31 +0800

    Development：改了一通名字

--- project/gitminingweb/WebContent/js/repo.js
+++ project/gitminingweb/WebContent/js/repo.js
@@ -1,58 +1,5 @@
-/*var repositories1 = [{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:58,forks:79,contributors:18},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:3,forks:12,contributors:2},
-{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:58,forks:79,contributors:18},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:3,forks:12,contributors:2},
-{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:58,forks:79,contributors:18},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:3,forks:12,contributors:2},
-{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:58,forks:79,contributors:18},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:3,forks:12,contributors:2},
-{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:58,forks:79,contributors:18},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:3,forks:12,contributors:2},
-{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:58,forks:79,contributors:18},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:3,forks:12,contributors:2},
-{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:58,forks:79,contributors:18},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:3,forks:12,contributors:2},
-{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:58,forks:79,contributors:18},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:3,forks:12,contributors:2}];
-
-var repositories2 = [{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:0,forks:0,contributors:0},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:0,forks:0,contributors:0},{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:0,forks:0,contributors:0},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:0,forks:0,contributors:0},{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:0,forks:0,contributors:0},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:0,forks:0,contributors:0},{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:0,forks:0,contributors:0},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:0,forks:0,contributors:0},{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:0,forks:0,contributors:0},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:0,forks:0,contributors:0},{fullName:"haha/ss",describe:"sldkfjslkdjflskdjflskdjflskdjflskdjf",
-lastUpdate:"2014-09-08",stars:0,forks:0,contributors:0},
-{fullName:"hdddd/soiusdf",describe:"ooowowoow",
-lastUpdate:"2016-02-07",stars:0,forks:0,contributors:0}];*/
-
-var lang = ["All","Java","Ruby","Python","C","JavaScript","Perl","PHP","C++","html","shell","Objective-C","VIML","C#","EmacsList","Erlang","Lua","Clojure","css","Haskell","Scala","CommonLisp","R","Others"];
-var cata = ["All","ActiveRecord","API","app","CMS","Django","Emacs","framework","interface","IRC","JSON","library","Linux","Mac","management","OS","plugin","Rails","Redis","server","source","template","TextMate","tool","Web","website","Others"];
+var languages = ["All","Java","Ruby","Python","C","JavaScript","Perl","PHP","C++","html","shell","Objective-C","VIML","C#","EmacsList","Erlang","Lua","Clojure","css","Haskell","Scala","CommonLisp","R","Others"];
+var catagories = ["All","ActiveRecord","API","app","CMS","Django","Emacs","framework","interface","IRC","JSON","library","Linux","Mac","management","OS","plugin","Rails","Redis","server","source","template","TextMate","tool","Web","website","Others"];
 
 function GetQueryString(name) { 
 var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)","i"); 
@@ -60,12 +7,12 @@ var r = window.location.search.substr(1).match(reg);
 if (r!=null) return (r[2]); return null; 
 }
 
-var repotype=GetQueryString("type"); 
+/*var repotype=GetQueryString("type"); 
 if(repotype!=null)	repotype = decodeURIComponent(repotype); 
 else				repotype = "All"
 var language=GetQueryString("lang"); 
 if(language!=null)	language = decodeURIComponent(language); 
-else				language = "All"
+else				language = "All"*/
 
 // $(document).ready(function() {
 
@@ -76,8 +23,14 @@ var app = angular.module('test', ['tm.pagination']);
 
 
 app.controller('testCtrl', ['$scope', 'BusinessService', function ($scope, BusinessService) {
-	//配置分页基本参数
-	$scope.type = "no";
+	$scope.sorttype = "no";
+	$scope.languages = languages;
+	$scope.catagories = catagories;
+    $scope.language = "All";
+    $scope.catagory = "All";
+
+    $scope.search = "";
+
 	$scope.paginationConf = {
 	    	currentPage: 1,
 	    	itemsPerPage: 15
@@ -85,12 +38,12 @@ app.controller('testCtrl', ['$scope', 'BusinessService', function ($scope, Busin
 	var GetAllEmployee = function () {
 		
 		console.log("now get new repos");
-		var postData = {
+		var getAttribute = {
 			type:"data",
 			method:"paged",
 			page:$scope.paginationConf.currentPage,
 			numPerPage:$scope.paginationConf.itemsPerPage,
-			sort:$scope.type
+			sort:$scope.sorttype
 		}
 		
 		
@@ -98,19 +51,14 @@ app.controller('testCtrl', ['$scope', 'BusinessService', function ($scope, Busin
 			function(response) {
 				$scope.paginationConf.totalItems = response.numOfRepo;
 			});
-		BusinessService.list(postData).success(
+		BusinessService.list(getAttribute).success(
 			function(response) {
 				$scope.repos=response;
 			});
 		
 	}
     
-	$scope.langs = lang;
-	$scope.catas = cata;
-    $scope.language = language;
-    $scope.repotype = repotype;
-
-    $scope.search = "";
+	
 
     $scope.getRepo = function(text) {
 		console.log(text);
@@ -122,24 +70,25 @@ app.controller('testCtrl', ['$scope', 'BusinessService', function ($scope, Busin
     	isFork:false,
     };
 
-
-	$scope.changetype = function(text) {
-		repotype = text;
-		console.log(language);
-		window.location.href = "test.html?type="+repotype+"&lang="+language;
-	};
-	$scope.formattime = function(text) {
+    $scope.formattime = function(text) {
 		return text.replace("T"," ").replace("Z"," ");
 	};
+
+	$scope.changetype = function(text) {
+		$scope.catagory = text;
+		// console.log(language);
+		// window.location.href = "test.html?type="+repotype+"&lang="+language;
+	};
+	
     $scope.changelang = function(text) {
-		language = text;
-		console.log(language);
-		window.location.href = "test.html?type="+repotype+"&lang="+language;
+		$scope.language = text;
+		// console.log(language);
+		// window.location.href = "test.html?type="+repotype+"&lang="+language;
 	};
 
 
     $scope.changerepo = function(retype) {
-    	$scope.type = retype;
+    	$scope.sorttype = retype;
     	$scope.paginationConf.currentPage = 1
     	if(retype=="no"){
     		$scope.isActive.isGen=true;
@@ -160,17 +109,17 @@ app.controller('testCtrl', ['$scope', 'BusinessService', function ($scope, Busin
     当页码和页面记录数发生变化时监控后台查询
     如果把currentPage和itemsPerPage分开监控的话则会触发两次后台事件。 
     ***************************************************************/
-    $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage + type', GetAllEmployee);}]);
+    $scope.$watch('paginationConf.currentPage + paginationConf.itemsPerPage + sorttype', GetAllEmployee);}]);
 //业务类
 app.factory('BusinessService', ['$http', function ($http) {
 	var url = "/GitMiningServer/repo";
-	var list = function (postData) {
+	var list = function (getAttribute) {
 		console.log("now change business");
-		console.log(postData);
+		console.log(getAttribute);
 		return $http({
 			 method:'GET',
 			 url:url,
-			 params:postData
+			 params:getAttribute
 			 });
     }
 	var getTotal = function () {
@@ -183,8 +132,8 @@ app.factory('BusinessService', ['$http', function ($http) {
     }
 
     return {
-    	list: function (postData) {
-    		return list(postData);
+    	list: function (getAttribute) {
+    		return list(getAttribute);
     	},
     	initial:function(){
     		return getTotal();
