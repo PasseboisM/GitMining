2e699aaa2bf8099ad086cbc108f675c6ae3c053d
xjyi14 <xjyi14@software.nju.edu.cn>
2016-06-02 20:41:16 +0800

    Other:雷达图重命名，为动态数据做准备

--- project/gitminingweb/WebContent/js/repoRadar.js
+++ project/gitminingweb/WebContent/js/repoRadar.js
@@ -1,4 +1,3 @@
-
 // require.config({
 //     paths: {
 //         echarts: 'http://echarts.baidu.com/build/dist'
@@ -9,31 +8,41 @@ var myChart = echarts.init(document.getElementById('qwe'));
 
 var option = {
     title: {
-        text: '基础雷达图'
+        text: '项目评分'
     },
     tooltip: {},
     legend: {
-        data: ['预算分配（Allocated Budget）']
+        data: ['score']
     },
     radar: {
         // shape: 'circle',
-        indicator: [
-           { name: '销售（sales）', max: 6500},
-           { name: '管理（Administration）', max: 16000},
-           { name: '信息技术（Information Techology）', max: 30000},
-           { name: '客服（Customer Support）', max: 38000},
-           { name: '研发（Development）', max: 52000},
-           { name: '市场（Marketing）', max: 25000}
-        ]
+        indicator: [{
+            name: 'famous',
+            max: 10
+        }, {
+            name: 'size',
+            max: 10
+        }, {
+            name: 'hot',
+            max: 10
+        }, {
+            name: 'popular',
+            max: 10
+        }, {
+            name: 'mature',
+            max: 10
+        }, {
+            name: 'contributor',
+            max: 10
+        }]
     },
     series: [{
-        name: '预算 vs 开销（Budget vs spending）',
+        name: '项目评分',
         type: 'radar',
         // areaStyle: {normal: {}},
-        data : [
-            {
-                value : [4300, 10000, 28000, 35000, 50000, 19000],
-                name : '预算分配（Allocated Budget）'
+        data: [{
+                value: [4, 7, 8, 3, 5, 9],
+                name: 'score'
             },
 
         ]
