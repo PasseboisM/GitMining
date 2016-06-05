05665e59a414d89db1339f972b5b2a9bc9b26273
wza14 <wza14@software.nju.edu.cn>
2016-06-05 22:49:31 +0800

    Development：格式化日期

--- project/gitminingweb/WebContent/js/userDetail.js
+++ project/gitminingweb/WebContent/js/userDetail.js
@@ -6,19 +6,19 @@ if (r!=null) return (r[2]); return null;
 
 var login=GetQueryString("login"); 
 console.log(login);
-var formattime = function(text) {
-	return text.replace("T"," ").replace("Z"," ");
-};
+
 // if(repotype!=null)	repotype = decodeURIComponent(repotype); 
 // else				repotype = null;
 var app = angular.module('detail_app', []);
 app.controller('detail_controller', function($scope, $http) {
-	var url = '/GitMiningServer/user', 
+	var url = '/GitMiningServer/user';
 	data = {
 		type : "data",
 		method: "spec",
 		param: login
 	};
+	
+	
 
 	$http({
 		method:'GET',
@@ -27,6 +27,9 @@ app.controller('detail_controller', function($scope, $http) {
 	}).success(function(data) {
 		$scope.user = data;
 		console.log(data);
+		$scope.formattime = function(text) {
+			return text.replace("T"," ").replace("Z"," ");
+		};
 	});
 
 });
