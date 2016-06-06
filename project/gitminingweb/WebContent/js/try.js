76dfb623ef9fb8bdf18773d0d6ead97f93aa269f
xjyi14 <xjyi14@software.nju.edu.cn>
2016-06-06 18:19:15 +0800

    Development:先假数据实现提交一下吧。。。

---
+++ project/gitminingweb/WebContent/js/try.js
@@ -0,0 +1,25 @@
+var app = angular.module('repo_app', []);
+
+
+app.controller('repo_ctrl', function($scope) {
+$scope.repo={
+   getStargazers_count:1,
+   getForks_count:2,
+   getSubscribers_count:3,
+
+   getUpdated_at: "1111T-2222Z",
+   getCreated_at: "11111T-22222Z",
+   getPushed_at:"1111111T-222222222222Z",
+   getSize: 10086,
+   getMainLanguage: "Brain F",
+   getUrl : "WWWWWWWWWWWWW",
+   getFull_name: "sdflkgjdg",
+   getName:"asdfasf",
+   getDescription:"sdlgksfl;skfjlsfkj;fjfj"
+};
+$scope.formattime = function(text) {
+  return text.replace("T"," ").replace("Z"," ");
+};
+
+
+});
