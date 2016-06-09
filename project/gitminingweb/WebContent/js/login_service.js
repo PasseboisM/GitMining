1267748dda2aa409ee606afd71cfa03f4ec5d00c
xjh14 <xjh14@software.nju.edu.cn>
2016-06-09 11:20:14 +0800

    Debug: 修复搜索Repo时Category参数处理异常的问题

--- project/gitminingweb/WebContent/js/login_service.js
+++ project/gitminingweb/WebContent/js/login_service.js
@@ -46,7 +46,7 @@ angular.module('main_app').factory('LoginService', ['$http', function ($http) {
     	save_cookie : function(text){
     		// var expirydate=new Date();
     		// expirydate.setTime(expirydate.getTime()+(1000*60*60*24*10))
-    		setCookie("key",text,-1);
+    		setCookie("key",text);
     	}
     }
 }]);
