8cfa1adff73b02e9c5f57ae749e0a0c66e5af7d4
wza14 <wza14@software.nju.edu.cn>
2016-06-09 16:43:37 +0800

    Development：RepoDetail添加登录相关代码

--- project/gitminingweb/WebContent/js/repoDetail.js
+++ project/gitminingweb/WebContent/js/repoDetail.js
@@ -11,7 +11,16 @@ var app = angular.module('main_app', []);
 app.controller('repo_ctrl','LoginService', function($scope, $http,LoginService) {
 
 	$scope.email = "";
-    $scope.password = "";
+    $scope.password = LoginService.get_cookie("email");
+    $(document).ready(function(){
+    	if($scope.email.length>0){
+    		$("#logout_div").show();
+    		$("#login_div").hide();
+    	}else{
+    		$("#login_div").show();
+    		$("#logout_div").hide();
+    	}
+    });
   	var url = '/GitMiningServer/repo';
 	data = {
 		type : "data",
@@ -21,6 +30,37 @@ app.controller('repo_ctrl','LoginService', function($scope, $http,LoginService) 
 
 	if(document.cookie.length>0)
 		data.key=LoginService.get_cookie("key");
+
+	$scope.login = function(){
+		param={
+			login:$scope.email,
+			pass:$scope.password
+		}
+		LoginService.login(param).success(
+			function(response) {
+				console.log(response);
+				if(response.state){
+					LoginService.save_cookie("key",response.key);
+					LoginService.save_cookie("email",$scope.email);
+					$("#login_div").hide();
+					$("#logout_div").show();
+					//show user info
+				}else{
+					//show alert info
+					console.log("hey sth. wrong!");
+					$scope.email="";
+					$scope.password="";
+				}
+			});
+	}
+	$scope.logout = function(){
+		LoginService.del_cookie("key");
+		LoginService.del_cookie("email");
+		$scope.email="";
+		$scope.password="";
+		$("#logout_div").hide();
+		$("#login_div").show();
+	}
 
 	$http({
 		method:'GET',
